75047d57af1e537b115807a0487924ba2f40fbc7
deltaking4000 <dblewisjr@outlook.com>
2025-12-28 15:14:48 -0800

    Port Changes and Tracking Wheels

--- Push Back Match/include/subsystems.hpp
+++ Push Back Match/include/subsystems.hpp
@@ -6,11 +6,11 @@
 extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
-inline pros::MotorGroup intake({-5, -6});  // Negative port will reverse the motor
+inline pros::MotorGroup intake({-5});  // Negative port will reverse the motor
 inline ez::Piston matchloader('A');
-inline pros::Motor lowerintake(-5);  // Negative port will reverse the motor
-inline pros::Motor upperintake(-6);  // Negative port will reverse the motor
-inline pros::Motor outtake(-7);
+inline pros::Motor lowerintake(-11);  // Negative port will reverse the motor
+inline pros::Motor upperintake(-12);  // Negative port will reverse the motor
+inline pros::Motor outtake(-6);
 inline ez::Piston wingmech('B');
  
 // inline pros::Motor intake(1);
